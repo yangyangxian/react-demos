bf9e1ba98698bba09a1426aff19599fd150f92f0
Nick Yang <Nick.Yang@activenetwork.com>
2017-11-07 18:00:02 +0800

    Initial commit

---
+++ libs/js/react-libs-cdn.js
@@ -0,0 +1,3 @@
+document.write("<script language=javascript src='https://unpkg.com/react@16/umd/react.development.js'></script>");
+document.write("<script language=javascript src='https://unpkg.com/react-dom@16/umd/react-dom.development.js'></script>");
+document.write("<script language=javascript src='https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js'></script>");
